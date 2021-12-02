6ceaf76b665f9ce006487d392ed2ec823d1c8ee2
yilmaz <yilmaz@newdirection.de>
2021-12-02 08:33:35 +0100

    First part of first day

---
+++ 1.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import sys
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
+
+depth_measurements = np.loadtxt("input_1", dtype=np.int32)
+
+# Part 1
+
+# print(depth_measurements[:10])
+# print(np.diff(depth_measurements)[:10])
+# print((np.diff(depth_measurements) > 0)[:10])
+
+print((np.diff(depth_measurements) > 0).sum())
+
+# Part 2
+
