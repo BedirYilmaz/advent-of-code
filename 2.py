5e601d040c5471c2edcc8cac249ea39bd25fca5e
yilmaz <yilmaz@newdirection.de>
2021-12-04 15:44:34 +0100

    Second start of Day 2 completed

--- 2.py
+++ 2.py
@@ -10,3 +10,18 @@ down = data.where(data[0]=="down")[1].sum()
 
 depth = down - up
 print(int(forward * depth))
+
+
+aim = 0
+pos_x = 0 
+depth = 0
+for index, row in data.iterrows():
+    if row[0] == "forward":
+        pos_x += row[1]
+        depth += row[1] * aim
+    elif row[0] == "up":
+        aim -= row[1]
+    elif row[0] == "down":
+        aim += row[1]
+
+print(int(depth * pos_x))
